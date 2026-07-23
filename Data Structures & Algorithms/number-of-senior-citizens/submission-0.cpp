de28713e35c767c21183dec61241bbd26c9357b8
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-07-23 20:47:41 +0545

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        
+        int count = 0;
+
+        for (string person : details) {
+            int age = (person[11] - '0') * 10 + (person[12] - '0');
+
+            if (age > 60) {
+                count ++;
+            }
+        }
+        return count;
+    }
+};
