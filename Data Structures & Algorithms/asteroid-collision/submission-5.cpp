85fa682cca141c3cbce4a2555df7b677855d2d3f
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-07-30 22:56:56 +0545

    Add: asteroid-collision - submission-5

---
+++ Data Structures & Algorithms/asteroid-collision/submission-5.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+
+        for (int asteroid : asteroids) {
+
+            bool alive = true;
+
+            while (alive && asteroid < 0 &&
+                   !st.empty() && st.top() > 0) {
+
+                if (st.top() < -asteroid) {
+                    st.pop();
+                }
+                else if (st.top() == -asteroid) {
+                    st.pop();
+                    alive = false;
+                }
+                else {
+                    alive = false;
+                }
+            }
+
+            if (alive) {
+                st.push(asteroid);
+            }
+        }
+
+        vector<int> ans(st.size());
+
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
